1ede59172d3df92e2e1bee4765dcfdac09cbaab2
binnur.kurt <binnur.kurt@gmail.com>
2020-01-10 11:13:28 +0300

    initial commit

---
+++ mastermind-boot/src/main/java/com/example/mastermind/MastermindBootApplication.java
@@ -0,0 +1,12 @@
+package com.example.mastermind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MastermindBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MastermindBootApplication.class, args);
+	}
+}
